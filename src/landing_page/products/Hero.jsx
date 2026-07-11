42e6146ce705be476c087644d63d1c24ea2e1d0c
piyushvohra25 <piyushvohra25@gmail.com>
2026-07-11 18:19:32 +0530

    Products Completed

--- src/landing_page/products/Hero.jsx
+++ src/landing_page/products/Hero.jsx
@@ -1,6 +1,12 @@
 function Hero () {
     return (  
-        <h1>Hero</h1>
+        <div className="container mt-4 border-bottom p-5">
+            <div className="row text-center">
+                <h1>Zerodha Products</h1>
+                <h3 className="text-muted mt-3">Sleek, modern, and intuitive trading platforms</h3>
+                <p className="mt-3 mb-3">Check out our <a href="" style={{textDecoration:"none"}}>investment offerings <i class="fa-solid fa-right-long"></i></a></p>
+            </div>
+        </div>
     );
 }
 
--- src/landing_page/products/RightSection.jsx
+++ src/landing_page/products/RightSection.jsx
@@ -1,7 +1,20 @@
-function RightSection () {
-    return (  
-        <h1>RightSection</h1>
+function RightSection({ imageURL, productName, productDescription, learnMore }) {
+    return (
+        <div className="conatiner">
+            <div className="row me-5 ms-5 pe-5 ps-5">
+                <div className="col-6 mt-5 p-5">
+                    <h1>{productName}</h1>
+                    <p className="text-muted mt-4 fs-5">{productDescription}</p>
+                    <div className="mt-4">
+                        <a href={learnMore} style={{ textDecoration: "none" }}>Learn more <i class="fa-solid fa-right-long"></i></a>
+                    </div>
+                </div>
+                <div className="col-6 pb-5">
+                    <img src={imageURL} style={{ width: "80%" }} alt="" />
+                </div>
+            </div>
+        </div>
     );
 }
 
-export default RightSection ;
+export default RightSection;
