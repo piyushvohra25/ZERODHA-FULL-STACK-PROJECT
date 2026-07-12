652b1da130324456b8457eb0eb23a15eb2663bf0
piyushvohra25 <piyushvohra25@gmail.com>
2026-07-12 18:32:33 +0530

    Support Completed

--- src/landing_page/support/Hero.jsx
+++ src/landing_page/support/Hero.jsx
@@ -1,7 +1,41 @@
-function Hero () {
-    return (  
-        <h1>Hero</h1>
+function Hero() {
+    return (
+        <header id="supportTab">
+            <div className="container" id="supportContainer">
+                <div className="row p-4">
+                    <div className="col-6">
+                        <h5>Support Portal</h5>
+                    </div>
+                    <div className="col-6" style={{textAlign:"right"}}>
+                        <a href="">Track Tickets</a>
+                    </div>
+                </div>
+                <div className="row m-3 pb-5">
+                    <div className="col-6">
+                        <h3>Search for an answer or browse help topics to create a ticket</h3>
+                        <input placeholder="Eg. how do I activate F&O" className="mb-3"/>
+                        <br />
+                        <a href="">Track account opening</a>
+                        <span>&nbsp; &nbsp; &nbsp; &nbsp;</span>
+                        <a href="">Track segment activation</a> 
+                        <span>&nbsp; &nbsp; &nbsp; &nbsp;</span>
+                        <a href="">Intraday margins</a> 
+                        <span>&nbsp; &nbsp; &nbsp; &nbsp;</span>
+                        <a href="">Kite user manual</a>
+                    </div>
+                    <div className="col-2">
+
+                    </div>
+                    <div className="col-4">
+                        <h3 className="mb-4">Featured</h3>
+                        <a href="">1. Current Takeovers and Delisting - January 2026</a> 
+                        <br /><br />
+                        <a href="">2. Latest Intraday leverages - MIS & CO</a>
+                    </div>
+                </div>
+            </div>
+        </header>
     );
 }
 
-export default Hero ;
+export default Hero;
--- src/landing_page/support/SupportPage.jsx
+++ src/landing_page/support/SupportPage.jsx
@@ -1,5 +1,3 @@
-import Footer from "../Footer";
-import Navbar from "../Navbar";
 import CreateTicket from "./CreateTicket";
 import Hero from "./Hero";
 
